dff9ed2f06e3c9aefce6c703dba6f791ce494b9f
pabitraadhikari@gmail.com <pabitraadhikari@gmail.com>
2014-04-11 10:26:51 -0700

    shell variable manupulation

---
+++ variable-manupulation.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+salutation="hello"
+echo $salutation
+echo "the program $0 is now running"
+echo "the second parameter was $2"
+echo "the first parameter was $1"
+echo "the parameter list was $*"
+echo "the user's home directory is $HOME"
+echo "the script is now complete"
+exit 0
+
+
+
